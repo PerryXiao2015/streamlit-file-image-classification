d388699f04a2ad26487057dc96e29c94558d461b
Dr Perry Xiao <xiaop@lsbu.ac.uk>
2020-10-22 14:35:09 +0100

    Update StreamlitFileImageClassification.py

--- StreamlitFileImageClassification.py
+++ StreamlitFileImageClassification.py
@@ -181,6 +181,6 @@ def main():
         st.subheader("About Image Classification App")
         st.markdown("Built with Streamlit by [LSBU](https://www.lsbu.ac.uk/)")
         st.text("Professor Perry Xiao")
-        st.success("Jesus Saves @PerryXiao")
+        st.success("Copyright @ 2020 London South Bank University")
 if __name__ == '__main__':
     main()	
